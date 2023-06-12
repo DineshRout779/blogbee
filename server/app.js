a0d3f981172829397a949f4d4d34b1b17a243221
Dinesh Rout <rddinesh779@gmail.com>
2023-06-13 01:02:02 +0530

    added routes api endpoints

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = express();
 const cors = require('cors');
 const multer = require('multer');
+const authRoutes = require('./routes/auth');
+const postsRoutes = require('./routes/posts');
+const usersRoutes = require('./routes/users');
 const connectDB = require('./config/db');
 dotenv.config();
 
@@ -14,9 +16,9 @@ app.use(cors());
 
 connectDB();
 
-app.get('/', (req, res) => {
-  res.send('working!');
-});
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/users', usersRoutes);
+app.use('/api/v1/posts', postsRoutes);
 
 app.listen(port, () =>
   console.log(`Server started on http://localhost:${port}`)
