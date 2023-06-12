4e3e8b390107440c7283e04f50b3abb30fd2e016
Dinesh Rout <rddinesh779@gmail.com>
2023-06-13 00:52:28 +0530

    Schema added: Post

---
+++ server/models/Post.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const PostSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+      default: '',
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    categories: {
+      type: Array,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Post', PostSchema);
