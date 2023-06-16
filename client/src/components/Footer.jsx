460507ddbe687aedb64af307d6446e7515b2133d
Dinesh Rout <rddinesh779@gmail.com>
2023-06-17 01:29:01 +0530

    section: footer ➕

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
-import { Box, Container, Flex, Image, Text, VStack } from '@chakra-ui/react';
+import { Box, Container, Flex, Text, VStack } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
-import darkLogo from '../assets/dark-logo.svg';
+import Logo from './Logo';
 
 const Footer = () => {
   return (
@@ -8,12 +8,7 @@ const Footer = () => {
       <Flex direction={{ base: 'column', sm: 'row' }}>
         <Box flex={0.3}>
           <Link to='/'>
-            <Flex alignItems={'center'} gap='1.5'>
-              <Image width={'48px'} height={'48px'} src={darkLogo} alt='logo' />
-              <Text fontSize={'2xl'} fontWeight='semibold'>
-                CodeTales
-              </Text>
-            </Flex>
+            <Logo />
           </Link>
         </Box>
         <VStack spacing='3' p='4' flex='0.3' align={'left'}>
@@ -24,16 +19,7 @@ const Footer = () => {
           <Link to='/about'>Features</Link>
           <Link to='/about'>Contact</Link>
         </VStack>
-        <Box flex={0.3}>
-          <Link to='/'>
-            <Flex alignItems={'center'} gap='1.5'>
-              <Image width={'48px'} height={'48px'} src={darkLogo} alt='logo' />
-              <Text fontSize={'2xl'} fontWeight='semibold'>
-                CodeTales
-              </Text>
-            </Flex>
-          </Link>
-        </Box>
+        <Box flex={0.3}></Box>
       </Flex>
     </Container>
   );
