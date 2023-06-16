ca0e7421c4c74318bc49ae57fd4331ae16042290
Dinesh Rout <rddinesh779@gmail.com>
2023-06-17 01:23:03 +0530

    feat: switch themes ✨

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import theme from './theme.js';
+import Home from './pages/Home.jsx';
+import Blogs from './pages/Blogs.jsx';
+import Login from './pages/Login.jsx';
+import Signup from './pages/Signup.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'signup',
+        element: <Signup />,
+      },
+      {
+        path: 'blogs',
+        element: <Blogs />,
+      },
+    ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
