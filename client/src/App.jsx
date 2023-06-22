48a34ac201cb0ec13ecfcc6b264446c01364cf6f
Dinesh Rout <rddinesh779@gmail.com>
2023-06-23 02:19:29 +0530

    feat: added toasters and scroll to top (on page change)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import { Box, useColorModeValue } from '@chakra-ui/react';
-import { Outlet } from 'react-router-dom';
+import { useLayoutEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
+  const location = useLocation();
   const bg = useColorModeValue('white', 'blackAlpha.500');
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
   return (
     <Box bg={bg}>
       <Outlet />
+      <Toaster position='bottom-left' reverseOrder={false} />
     </Box>
   );
 }
