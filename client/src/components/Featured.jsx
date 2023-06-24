116f2f08812b17433535ff98a07977ca909f71e4
Dinesh Rout <rddinesh779@gmail.com>
2023-06-24 17:40:21 +0530

    sync with server data: blog cards

--- client/src/components/Featured.jsx
+++ client/src/components/Featured.jsx
@@ -1,20 +1,43 @@
-import { Container, Flex, Stack, Text } from '@chakra-ui/react';
+import { Container, Flex, Spinner, Stack, Text } from '@chakra-ui/react';
+import { useBlogs } from '../hooks/useBlogs';
 import BlogCard from './BlogCard';
 import BlogCardHorizontal from './BlogCardHorizontal';
 
 const Featured = () => {
+  const {
+    state: { blogs, loading, error },
+  } = useBlogs();
+
+  if (loading) {
+    return (
+      <Flex
+        minH={'100vh'}
+        flexDir='column'
+        justifyContent='center'
+        alignItems={'center'}
+      >
+        <Spinner size='lg' />
+        <Text my='4'>Fetching blogs...</Text>
+      </Flex>
+    );
+  }
+
+  if (error) {
+    return <p>Some error occurred!</p>;
+  }
+
   return (
     <Container maxWidth={'1200px'} my='12'>
       <Text as={'h1'} my='8' fontWeight='semibold' fontSize={'2xl'}>
         Featured Posts
       </Text>
       <Flex gap='2em' direction={{ base: 'column', md: 'row' }}>
-        <BlogCard />
+        <BlogCard blog={blogs[0]} />
 
         <Stack spacing={'4'} flex={'0.6'}>
-          <BlogCardHorizontal />
-          <BlogCardHorizontal />
-          <BlogCardHorizontal />
+          {blogs.map((blog) => (
+            <BlogCardHorizontal blog={blog} key={blog._id} />
+          ))}
         </Stack>
       </Flex>
     </Container>
