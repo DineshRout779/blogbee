711ea513e99faae0c609f6c4c153603f5b703d96
Dinesh Rout <rddinesh779@gmail.com>
2023-06-24 17:36:01 +0530

    feat: update view count

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -110,6 +110,28 @@ exports.updatePostById = async (req, res) => {
   }
 };
 
+// update views of a post
+exports.updatePostViews = async (req, res) => {
+  try {
+    const post = await Post.findOne({ _id: req.post._id, views: req.user._id });
+    if (post) {
+      // User has already viewed the post
+      return res
+        .status(200)
+        .json({ message: 'View count already incremented' });
+    }
+    await Post.findByIdAndUpdate(req.post._id, {
+      $push: { views: req.user._id },
+    });
+    res.json({ message: 'View count incremented successfully' });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      error,
+    });
+  }
+};
+
 // delete post
 exports.deletePostById = async (req, res) => {
   const post = await Post.findById(req.post._id);
